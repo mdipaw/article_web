c2e37e3d27193b05fd8dd76af0ad74c804c9680f
difaagh <dhifa.aghisni@gmail.com>
2022-06-20 01:27:38 +0700

    add test helper

---
+++ tests/setup.go
@@ -0,0 +1,41 @@
+package tests
+
+import (
+	"article_web/database"
+	"article_web/migrations"
+	"sync"
+
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var doOnce sync.Once
+
+type testFunc func(*gorm.DB, *gorm.DB)
+
+func NewDBTest(testFunc testFunc) {
+	dbReader, dbWriter := setUpDB()
+	db1, _ := dbReader.DB()
+	db2, _ := dbWriter.DB()
+
+	defer dbWriter.Rollback()
+	defer dbReader.Rollback()
+	defer db1.Close()
+	defer db2.Close()
+
+	testFunc(dbReader, dbWriter)
+
+}
+
+func setUpDB() (tx, tr *gorm.DB) {
+	db := database.ConnectDatabaseReader(database.DsnReader, logger.Default.LogMode(logger.Error))
+	db2 := database.ConnectDatabaseWriter(database.DsnWriter, logger.Default.LogMode(logger.Info))
+
+	doOnce.Do(func() {
+		migrations.Up(db)
+		migrations.Up(db2)
+	})
+	tx = db.Begin()
+	tr = db2.Begin()
+	return
+}
